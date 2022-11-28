d52a79e12a4e0f7a0071231808a2a86beb22e71f
isaac <isaactonyloi@gmail.com>
2022-11-28 11:04:49 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "CodeMojo";
+
+function upperCaseCustomerName(customerName) {
+    return cus.toUppercase();
+}
+
+
+function setBestCustomer() {
+    return  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = "Tonyloi";
+}
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+    return leastFavoriteCustomer = "CodeBantu";
+}
